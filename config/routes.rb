421902f213dcbebfc6b66307024ab3cc13195de0
EC2 Default User <ec2-user@ip-172-31-1-21.ap-northeast-1.compute.internal>
2023-09-10 05:05:00 +0000

    投稿未

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
-  resources :posts, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
+  resources :posts, only: [:index, :show, :edit, :update, :destroy] do
     resources :post_comments, only: [:create, :destroy]
   end
   resources :users, only: [:new, :create, :show, :edit, :update, :destroy] do
@@ -11,8 +11,8 @@ Rails.application.routes.draw do
       # resources :post_comments, only: [:create, :destroy]
   end
   get '/user/:user_id/check' => 'users#check', as: 'check'
-  
-  
+  get '/children/:id/post/:id' => 'posts#new', as: 'post/:id'
+  post 'children/:idpost' => 'posts#create',as: 'post'
   # get '/user_id/childs/:id' => 'children#show'
   
 end
