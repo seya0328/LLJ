d6ce5e9e45747438d2a0857cbefb8ca8eb9fccce
EC2 Default User <ec2-user@ip-172-31-1-21.ap-northeast-1.compute.internal>
2023-10-08 11:24:10 +0000

    Update

--- db/migrate/20230916061807_add_missing_indexes_on_taggings.acts_as_taggable_on_engine.rb
+++ db/migrate/20230916061807_add_missing_indexes_on_taggings.acts_as_taggable_on_engine.rb
@@ -5,11 +5,11 @@ class AddMissingIndexesOnTaggings < ActiveRecord::Migration[6.0]
   def change
     add_index ActsAsTaggableOn.taggings_table, :tag_id unless index_exists? ActsAsTaggableOn.taggings_table, :tag_id
     add_index ActsAsTaggableOn.taggings_table, :taggable_id unless index_exists? ActsAsTaggableOn.taggings_table,
-                                                                                 :taggable_id
+                                                                                :taggable_id
     add_index ActsAsTaggableOn.taggings_table, :taggable_type unless index_exists? ActsAsTaggableOn.taggings_table,
-                                                                                   :taggable_type
+                                                                                  :taggable_type
     add_index ActsAsTaggableOn.taggings_table, :tagger_id unless index_exists? ActsAsTaggableOn.taggings_table,
-                                                                               :tagger_id
+                                                                              :tagger_id
     add_index ActsAsTaggableOn.taggings_table, :context unless index_exists? ActsAsTaggableOn.taggings_table, :context
 
     unless index_exists? ActsAsTaggableOn.taggings_table, %i[tagger_id tagger_type]
